003b1285f1ded59d91444396edf1e20192e55ef3
longTTTB <nolg8429@gmail.com>
2025-06-04 14:14:06 +0700

    new

---
+++ Assets/Setting/ChangeImageButton.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ChangeImageButton : MonoBehaviour
+{
+    public Sprite offImage;
+    public Sprite onImage;
+    public Button button;
+    private bool isOn = true;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void ChangeButtonImage()
+    {
+        if(isOn)
+        {
+            button.image.sprite = offImage;
+            isOn = false;
+        }
+        else
+        {
+            button.image.sprite = onImage;
+            isOn=true;
+        }
+        
+    }
+}
